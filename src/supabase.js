e4d173140ae75aec3dd99331af509a8c6099d1bb
Digital Farms <soulstream2012@gmail.com>
2025-02-26 20:03:40 +0200

    Clean Project

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://kozbsjeqafhthbwekhjl.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvemJzamVxYWZodGhid2VraGpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNjUyNTIsImV4cCI6MjA1NTc0MTI1Mn0.jjwhtP3qcpI7bsEUXatGQ3OzTXwD6tqCHw9a6MC5d5k'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
